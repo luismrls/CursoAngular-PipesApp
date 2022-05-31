fd04b586405522c85c7bdf450fbe9864d34c1a9b
Luis Morales <dev.luismorales@gmail.com>
2022-05-31 08:42:29 -0500

    se cambió el idioma de la aplicación

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
@@ -8,7 +8,11 @@ import { AppRouterModule } from './app-router.module';
 import { SharedModule } from './shared/shared.module';
 import { VentasModule } from './ventas/ventas.module';
 
+// Cmabiar el locale de la app
+import localeEs from '@Angular/common/locales/es-CO';
+import { registerLocaleData } from '@angular/common';
 
+registerLocaleData( localeEs );
 
 @NgModule({
   declarations: [
@@ -20,7 +24,9 @@ import { VentasModule } from './ventas/ventas.module';
     SharedModule,
     VentasModule
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'es-CO' }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
