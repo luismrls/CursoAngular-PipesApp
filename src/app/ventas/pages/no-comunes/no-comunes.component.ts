21ae8a6b5bcabd4d9f54da37eaf44438c9b80f77
Luis Morales <dev.luismorales@gmail.com>
2022-05-31 11:58:11 -0500

    se aprendio el uso de pipe i18nPlural

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -8,12 +8,22 @@ import { Component} from '@angular/core';
 })
 export class NoComunesComponent {
 
+  // i18nSelect
   nombre: string = 'Luis';
   genero: string = 'masculino';
 
   invitacionMapa = {
     'masculino': 'invitarlo',
     'femenino': 'invitarla'
+  }
+
+  // i18nPlural
+  clientes: string[] = ['Maria', 'Luis', 'Ximena', 'Xilena'];
+  clientesMapa = {
+    '=0': 'no tenemos ningun cliente esperando.',
+    '=1': 'tenemos 1 cliente esperando.',
+    '=2': 'tenemos 2 clientes esperando.',
+    'other': 'tenemos # clientes esperando.'
   }
 
 }
